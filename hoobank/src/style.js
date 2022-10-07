155abc4573d52f28f893b43fd7b7bedd8ccb0b56
rvitality <vital.renzjohn@gmail.com>
2022-10-07 17:38:31 +0800

    HooBank: Change screen sizes

--- hoobank/src/style.js
+++ hoobank/src/style.js
@@ -1,5 +1,5 @@
 const styles = {
-    boxWidth: "xl:max-w-[1280px] w-full",
+    boxWidth: "w-full 2xl:max-w-screen-2xl ",
 
     heading2:
         "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
@@ -8,20 +8,20 @@ const styles = {
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start",
 
-    paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-12 py-4",
+    paddingX: "md:px-12 lg:px-16 px-6",
+    paddingY: "md:px-12 lg:py-16 py-6",
+    padding: "md:px-12 lg:px-16 px-6 md:px-12 lg:py-12 py-4",
 
-    marginX: "sm:mx-16 mx-6",
-    marginY: "sm:my-16 my-6",
+    marginX: "md:px-12 lg:mx-16 mx-6",
+    marginY: "md:px-12 lg:my-16 my-6",
 };
 
 export const layout = {
-    section: `flex md:flex-row flex-col ${styles.paddingY}`,
-    sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+    section: `flex lg:flex-row flex-col ${styles.paddingY}`,
+    sectionReverse: `flex lg:flex-row flex-col-reverse ${styles.paddingY}`,
 
-    sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
-    sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+    sectionImgReverse: `flex-1 flex ${styles.flexCenter} lg:mr-10 mr-0 lg:mt-0 mt-10 relative`,
+    sectionImg: `flex-1 flex ${styles.flexCenter} lg:ml-10 ml-0 lg:mt-0 mt-10 relative`,
 
     sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };
